c50df76d7cf66ab9653bd46c77478d3bf9041509
Vlad <vlad09fedko@gmaul.com>
2026-04-05 13:07:57 +0300

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 'use strict ';
 
-let enteredNum = Number(prompt('Введіть цифру від 0 до 7'));
-switch (enteredNum) {
-  case 0:
+const ENTERED_NUM = prompt('Введіть цифру від 0 до 7').trim();
+switch (ENTERED_NUM) {
+  case '0':
     console.log('Рівень прав: ---');
     break;
-  case 1:
+  case '1':
     console.log('Рівень прав: --x');
     break;
-  case 2:
+  case '2':
     console.log('Рівень прав: -w-');
     break;
-  case 3:
+  case '3':
     console.log('Рівень прав: -wx');
     break;
-  case 4:
+  case '4':
     console.log('Рівень прав: r--');
     break;
-  case 5:
+  case '5':
     console.log('Рівень прав: r-x');
     break;
-  case 6:
+  case '6':
     console.log('Рівень прав: rw-');
     break;
-  case 7:
+  case '7':
     console.log('Рівень прав: rwx');
     break;
   default:
-    console.log(`${enteredNum} це некоретне значення`);
+    console.log(`${ENTERED_NUM} це некоретне значення`);
 }
